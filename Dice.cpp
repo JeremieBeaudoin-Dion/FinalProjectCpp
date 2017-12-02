7ad221bf68b341a4de323d3b809b608f71f79b70
JeremieBeaudoin-Dion <jeremie.beaudoin.dion@gmail.com>
2017-12-01 20:20:04 -0500

    Fixed the << of Dice

--- Dice.cpp
+++ Dice.cpp
@@ -8,6 +8,32 @@ void Dice::roll() {
 }
 
 std::ostream& operator<< (std::ostream& os, Dice& d) {
+	auto colour_as_integer = static_cast<int>(d.colour);
+	
+	switch(colour_as_integer) {
+		
+		case 0:
+			os << "Red dice: ";
+			break;
+		
+		case 1:
+			os << "Yellow dice: ";
+			break;
+			
+		case 2:
+			os << "Blue dice: ";
+			break;
+			
+		case 3:
+			os << "Green dice: ";
+			break;
+			
+		case 4:
+			os << "White dice: ";
+			break;
+	}
+	
 	os << d.face;
+	
     return os;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
 	
-	Colour r = Colour::RED;
+	Colour r = Colour::GREEN;
 	Dice d(r);
 	
 	d.roll();
